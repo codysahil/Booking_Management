d21b2b4f5660d22ee936dbb9b73504a7f9a34e7e
Aanchal Saxena <aanchalsaxenacontact@gmail.com>
2025-11-28 00:39:12 +0530

    changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,12 +49,12 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
+            'cloud' => env('CLOUDINARY_CLOUD_NAME'),
             'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'cloud_url' => env('CLOUDINARY_URL'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
             'key' => env('CLOUDINARY_API_KEY'),
             'secret' => env('CLOUDINARY_API_SECRET'),
+            'api_key' => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
             'secure' => true,
         ],
 
