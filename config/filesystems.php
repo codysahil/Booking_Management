e9945e521b3dc6c2c9d8167df72cf2f822279879
Aanchal Saxena <aanchalsaxenacontact@gmail.com>
2025-11-28 00:32:51 +0530

    changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -49,9 +49,13 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME', ''),
-            'api_key' => env('CLOUDINARY_API_KEY', ''),
-            'api_secret' => env('CLOUDINARY_API_SECRET', ''),
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'cloud_url' => env('CLOUDINARY_URL'),
+            'api_key' => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'key' => env('CLOUDINARY_API_KEY'),
+            'secret' => env('CLOUDINARY_API_SECRET'),
+            'secure' => true,
             'url' => [
                 'secure' => true,
             ],
